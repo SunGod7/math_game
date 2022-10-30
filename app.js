f8af34a6d738de2b6093e59d72d102f52f56a344
SunGod7 <sun7ruler@gmail.com>
2022-10-30 13:56:22 -0400

    drawing squares

--- app.js
+++ app.js
@@ -12,4 +12,23 @@ function main(){
     ctx.width = canvas.width
     ctx.height= canvas.height
 
-}
+
+    ctx.fillStyle = 'black'
+    ctx.strokeStyle = 'red'
+    ctx.lineWidth = 5
+
+    let x = 100
+    let y = 150
+    let wid = 200
+    let hei = 300
+
+    ctx.fillRect(x, y, wid, hei)
+    ctx.strokeRect(x, y, wid, hei)
+    //ctx.fillRect(50, 50, 80, 80)
+//     ctx.strokeRect(10, 10, 80, 80)
+
+//  ctx.beginPath()
+//  ctx.arc(400, 300, 90, 0, Math.PI = 2, false);
+//  ctx.strokeStyle = 'red'
+//  ctx.stroke()
+ }
