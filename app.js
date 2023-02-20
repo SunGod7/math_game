501ca8e0c5698ed158a4f19500b33ce62c5ad9b7
SunGod7 <sun7ruler@gmail.com>
2023-02-19 20:48:35 -0500

    player not showing up

--- app.js
+++ app.js
@@ -38,20 +38,23 @@ console.log('height', game.height)
 class KatCrawler {
 
 
-   constructor(x, y, color, width, height) {
+   constructor(x, y, color, width, height, type) {
       this.x = x,
          this.y = y,
          this.color = color,
          this.width = width,
          this.height = height,
-         // this.type = type,
-         this.alive = true,
-
-         this.render = function () {
-            ctx.fillStyle = this.color
-            ctx.fillRect(this.x, this.y, this.width, this.height)
+         this.type = type,
+         this.alive = true
+      }
+         // this.render = function () {
+         //    ctx.fillStyle = this.color
+         //    ctx.fillRect(this.x, this.y, this.width, this.height)
+         // }
+         render = function () {
+         ctx.fillStyle = this.color
+         ctx.fillRect(this.x, this.y, this.width, this.height)
          }
-   }
 }
 
 //------------------------------------------------//
@@ -64,7 +67,7 @@ class DoggyCrawler {
          this.color = color,
          this.width = width,
          this.height = height,
-         this.alive = true,
+         this.alive = true
 
          this.speed = 15,
          this.direction = {
@@ -81,81 +84,133 @@ class DoggyCrawler {
    setDirection = function (key) {// IM HERE*******
       console.log('this is the key thaty was pressed', key)
       if (key.toLowerCase() == 'w') { this.direction.up = true }
-      if (key.toLowerCase() == 'w') { this.direction.up = true }
-      if (key.toLowerCase() == 'w') { this.direction.up = true }
-      if (key.toLowerCase() == 'w') { this.direction.up = true }
-   }
-}
+      if (key.toLowerCase() == 'a') { this.direction.down = true }
+      if (key.toLowerCase() == 's') { this.direction.left = true }
+      if (key.toLowerCase() == 'd') { this.direction.right = true }
 
-let kat = new KatCrawler(200, 50, 'red', 32, 48, kat)
+      // if (e.keyCode() == '38') { this.direction.up = true }
+      // if (key.toLowerCase() == 'w') { this.direction.down = true }
+      // if (key.toLowerCase() == 'w') { this.direction.left = true }
+      // if (key.toLowerCase() == 'w') { this.direction.right = true }
+   }
+   unSetDirection = function (key){
+      if (key.toLowerCase() == 'w') { this.direction.up = false }
+      if (key.toLowerCase() == 'a') { this.direction.down = false }
+      if (key.toLowerCase() == 's') { this.direction.left = false }
+      if (key.toLowerCase() == 'd') { this.direction.right = false }
+   }
+
+   movePlayer = function (){
+      if (this.direction.up) {
+         this.y -= this.speed
+   }
+   if (this.direction.left) {
+      this.x -= this.speed
+   }
+   if (this.direction.down) {
+      this.y += this.speed
+   }
+   if (this.direction.right) {
+      this.x += this.speed
+   }
+  
+}
+ render = function (){
+      ctx.fillStyle = this.color
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+   }
+}  
+let kat = new KatCrawler(200, 50, 'red', 32, 48)
 let player = new DoggyCrawler(20, 20, '#bada55', 16, 14)
 
 const gameLoop = () => {
 
-   if (player.alive) {
+   if (kat.alive) {
       detectHit()
    }
    ctx.clearRect(0, 0, game.width, game.height)
-   kat.render()
+   movement.textContent = player.x + ', ' + player.y
+   player.render()
 
 
-   if (player.alive) {
-      player.render()
+   if (kat.alive) {
+      kat.render()
    }
+   player.render()
+   player.movePlayer()
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-   document.addEventListener('keydown', movementHandler)
+   //document.addEventListener('keydown', movementHandler)
    setInterval(gameLoop, 60)
 })
+//-----added------
+document.addEventListener('keydown', (e) =>{
+   player.setDirection(e.key)
+})
 
+document.addEventListener('keyup', (e) =>{
+   if (['w', 'a', 's', 'd'].includes(e.key)) {
 
-const movementHandler = (e) => {
-
-   switch (e.keyCode) {
-
-      case (38):
-         // this moves player up
-         player.y -= 10
-         // we also need to break the case
-         break
-
-      case (37):
-         // this moves the player left
-         player.x -= 10
-         break
-
-      case (40):
-         // this will move the player down
-         player.y += 10
-         break
-
-      case (39):
-         // this moves the player to the right
-         player.x += 10
-         break
+      player.unSetDirection(e.key)
    }
+})
+//-----added------
+//const movementHandler = (e) => {
+   //keycodes
+   // w = 87, a = 65, s = 83, d = 68
+   // up=38, down=40, left=37, right=39
+   // in order to do different things for different keys, we can use if statements, or we can use a switch case
+   // switch cases are handy when you have a multiple possibilities
+   // switch case has a main switch, and cases(in this our cases are inputs)
+   // we also need a break in every case, so 
+//    switch (e.keyCode) {
 
-}
+//       case (87): // this moves player up
+//       case (38):
+//          player.y -= 10
+//          // we also need to break the case
+//          break
+
+//       case (37):
+//       case (65):
+//          // this moves the player left
+//          player.x -= 10
+//          break
+
+//       case (40):
+//       case (83):
+//          // this will move the player down
+//          player.y += 10
+//          break
+
+//       case (39):
+//       case (68):
+//          // this moves the player to the right
+//          player.x += 10
+//          break
+//    }
+
+// }
 
 
-const detectHit = () => {
-   // we'll use one big if statement that clearly defines any moment of collision.
-   // that means utilizing, x, y, width and height of our objects
-   if (player.x < kat.x + kat.width
-      && player.x + player.width > kat.x
-      && player.y < kat.y + kat.height
-      && player.y + player.height > kat.y) {
-      // here we can see if the hit happens at the right time
-      // console.log('we have a hit')
-      // if a hit occurs, player dies 
-      player.alive = false
-      document.getElementById("btm-left-gameOver").textContent = 'You lose!!!!'
-      //   document.getElementById('status').textContent = 'You Win!'
+// const detectHit = () => {
+//    // we'll use one big if statement that clearly defines any moment of collision.
+//    // that means utilizing, x, y, width and height of our objects
+//    if (player.x < kat.x + kat.width
+//       && player.x + player.width > kat.x
+//       && player.y < kat.y + kat.height
+//       && player.y + player.height > kat.y) {
+//       // here we can see if the hit happens at the right time
+//       // console.log('we have a hit')
+//       // if a hit occurs, player dies 
+//       player.alive = false
+//       document.getElementById("btm-left-gameOver").textContent = 'You lose!!!!'
+//       //   document.getElementById('status').textContent = 'You Win!'
 
-   }
+//    }
 
-}
+//}
 
 console.log(game)
 
